aa52801ab5f01fb34fa1a1006b2d65ed4467c441
blocktest-official <43646822+blocktest-official@users.noreply.github.com>
2019-01-16 09:50:02 -0500

    Uploaded contracts

---
+++ TestVoteForSweaterPt3.sol
@@ -0,0 +1,30 @@
+pragma solidity 0.4.25;
+
+import "./VoteForSweaterPt3.sol";
+
+contract TestVoteForSweaterPt3 is VoteForSweaterPt3 {
+
+  function test_sweaterVote() {
+    // Voting for an invalid option 'turtle neck'
+    uint turtleneckVotes = 0;
+    bool voted = checkAlreadyVoted(msg.sender);
+    assert(getVotes("turtle neck") == turtleneckVotes);
+    assert(voted == false);
+    sweaterVote("turtle neck");
+    voted = checkAlreadyVoted(msg.sender);
+    assert(getVotes("turtle neck") == turtleneckVotes);
+    assert(voted == false);
+
+    // Voting for a valid option 'blue'
+    uint blueVotes = getVotes("blue");
+    assert(voted == false);
+    sweaterVote("blue");
+    blueVotes += 1;
+    assert(getVotes("blue") == blueVotes);
+    voted = checkAlreadyVoted(msg.sender);
+    assert(voted == true);
+
+    bool result = address(this).call(bytes4(keccak256("sweaterVote('green')")));
+    assert(result == false);
+  }
+}
